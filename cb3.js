2e888d7ce6793352cd8a42557e9cf97b34ed9ae2
t ddddddd <webdev@cmme.org>
2016-08-24 23:54:12 -0700

    terrible callback pyramid example

---
+++ cb3.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const delayedDouble = (val, cb) => {
+  const delayMS = Math.floor(Math.random() * 4000);
+  setTimeout(() => cb(val * 2), delayMS);
+};
+
+let totalLogged = 0;
+
+const logValue = val => {
+  console.log(val);
+  if (++totalLogged === 3) {
+    console.log('done!');
+  }
+};
+
+delayedDouble(1, v1 => {
+  logValue(v1);
+
+  delayedDouble(2, v2 => {
+    logValue(v2);
+
+    delayedDouble(3, v3 => {
+      logValue(v3);
+    });
+
+  });
+
+});
