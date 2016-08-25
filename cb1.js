cad8e46819adb50227203d64c3ceec20c998c96b
teddddd <ted@mixpanel.com>
2016-08-25 01:29:38 +0000

    3 callbacks

---
+++ cb1.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const delayedDouble = (val, cb) => {
+  const delayMS = Math.floor(Math.random() * 4000);
+  setTimeout(() => cb(val * 2), delayMS);
+};
+
+delayedDouble(1, val => console.log(val));
+delayedDouble(2, val => console.log(val));
+delayedDouble(3, val => console.log(val));
