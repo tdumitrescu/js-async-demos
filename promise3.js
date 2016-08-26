273a7846e03021c772ea9de1c38d6864ecf4460e
tdumitrescu <miscmisc@cmme.org>
2016-08-25 18:28:32 -0700

    more slide spacing

--- promise3.js
+++ promise3.js
@@ -4,7 +4,9 @@ let result;
 if (process.argv[process.argv.length - 1] === 'immediate') {
   result = 'immediate';
 } else {
-  result = new Promise(resolve => setTimeout(() => resolve('delayed'), 2000));
+  result = new Promise(resolve =>
+    setTimeout(() => resolve('delayed'), 2000)
+  );
 }
 
 Promise.resolve(result).then(v => console.log(v));
